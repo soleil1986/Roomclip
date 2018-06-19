25accf5abbe058dd53cede035ff5a25c588c3cf9
toshi-oliver <igpoid@gmail.com>
2018-06-19 17:45:27 +0900

    ルーティングの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    config.generators do |g|
-      g.stylesheets false
-      g.javascripts false
-      g.helper false
-      g.test_framework false
-    end
+
 end
