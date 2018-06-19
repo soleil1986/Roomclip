92601458bf5b4096ed991fb8521bd9d044207e41
ohidetoshi <onuki_hidetoshi@onukihidetoshinoMacBook-puro.local>
2018-06-19 18:30:13 +0900

    Clipのコントローラとモデルを編集

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -1,2 +1,26 @@
 class ClipsController < ApplicationController
+
+  def new
+    @clip = Clip.new
+  end
+
+  def create
+    @clip = Clip.new(clip_params)
+  end
+
+  def show
+    @clip = Clip.find(params[:id])
+  end
+
+  def destroy
+  end
+
+  private
+
+  def clip_params
+    params.require(:clip).permit(
+      :image
+    )
+  end
+
 end
--- app/models/clip.rb
+++ app/models/clip.rb
@@ -1,2 +1,7 @@
 class Clip < ApplicationRecord
+  belogns_to :user
+
+  def like_user(user_id)
+    likes.find_by(user_id: user_id)
+  end
 end
