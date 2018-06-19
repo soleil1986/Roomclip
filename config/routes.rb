784474b66e3edbdc0e42b975fbb14cd08f422e83
hiroaki <sanotaiyou@sanotaiyounoMacBook-puro.local>
2018-06-19 12:13:33 +0900

    不要なファイル群削除
    不要なファイルが生成されないようにするための記述追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
 end
-
-123
