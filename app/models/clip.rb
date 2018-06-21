78c5e5f016d1bef82d0db616dfd0fae4a8eaeaf2
ohidetoshi <onuki_hidetoshi@onukihidetoshinoMacBook-puro.local>
2018-06-21 21:31:22 +0900

    画像保存のため設定

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -2,6 +2,8 @@ class Clip < ApplicationRecord
 # マージする際にはコメントアウトを戻す
   # belogns_to :user
 
+  mount_uploader :image, ClipImageUploader
+
   def like_user(user_id)
     likes.find_by(user_id: user_id)
   end
