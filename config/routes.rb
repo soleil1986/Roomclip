a80b731c4cff0771f44a3c9d507184a3ebf5f58b
shukan0728 <cielo28verano@gmail.com>
2018-06-23 21:47:49 +0900

    共通フッダーの追加(ドロップダウンメニュー実装は後から追加)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root app/views/header.html.erb
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
