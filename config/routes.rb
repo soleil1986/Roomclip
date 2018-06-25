e8a2a3a3f63e8ebb57c319c62d323ca6791d1163
hiroaki <>
2018-06-25 18:49:13 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 # マージする前に不要な記述を削除する
-  resources :clips, only: [:new, :create]
+  root to: "clips#index"
+  resources :clips, only:[:index, :new, :create, :destroy, :edit, :update] do
+]
 end
