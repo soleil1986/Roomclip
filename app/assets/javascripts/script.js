a6cd8413d8102b2025a56c7eb960ab0a4ad109dc
hiroaki <>
2018-06-26 18:31:05 +0900

    indexページの配置

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,38 @@
+$(function(){
+		$("#parent").on("click", function() {
+			$("#child").slideToggle();
+		});
+    $("#parent2").on("click", function() {
+			$("#child2").slideToggle();
+		});
+    $("#parent3").on("click", function() {
+			$("#child3").slideToggle();
+		});
+    $("#parent4").on("click", function() {
+			$("#child4").slideToggle();
+		});
+    $("#parent5").on("click", function() {
+			$("#child5").slideToggle();
+		});
+    $("#parent6").on("click", function() {
+			$("#child6").slideToggle();
+		});
+    $("#parent7").on("click", function() {
+			$("#child7").slideToggle();
+		});
+    $("#parent8").on("click", function() {
+			$("#child8").slideToggle();
+		});
+    $("#parent9").on("click", function() {
+			$("#child9").slideToggle();
+		});
+    $("#parent10").on("click", function() {
+			$("#child10").slideToggle();
+		});
+    $("#parent11").on("click", function() {
+			$("#child11").slideToggle();
+		});
+    $("#parent12").on("click", function() {
+			$("#child12").slideToggle();
+		});
+	});
