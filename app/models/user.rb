8a8c6ce0cbb3da0684429e03b5e559745db3980d
toshi-oliver <igpoid@gmail.com>
2018-06-28 19:16:01 +0900

    deviseの入れ替え

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable
+         :recoverable, :rememberable, :trackable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :trackable, :validatable,
+  #        :confirmable, :lockable
 
-  validates :name, presence: true
-  validates :email, uniqueness: true
-  validates :password, confirmation: true
-
+  # validates :name, presence: true
+  # validates :email, uniqueness: true
+  # validates :password, confirmation: true
 end
