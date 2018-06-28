e03c5c5148f8becb7a146c6cc46b8e95930c667e
hiroaki <sanotaiyou@sanotaiyounoMacBook-puro.local>
2018-06-28 15:26:19 +0900

    ルーティングの設定
    マイルーム(ユーザの詳細)ページへ遷移するように記述

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 # マージする前に不要な記述を削除する
   root "clips#index"
+  resources :users, only: [:show]
   resources :clips, only:[:index, :new, :create, :destroy, :show] do
   end
 end
