2c52da62675aae29c06740898d35afad5d3bc338
toshi-oliver <igpoid@gmail.com>
2018-06-30 18:46:03 +0900

    いいねの数をカウントするためにcounter_cacheを追加

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-  belong_to :user
-  belong_to :clip, counter_cache: likes_count
+  belong_to :user, counter_cache: :likes_count
+  belong_to :clip, counter_cache: :likes_count
 
   validates :user_id, presence: true
   validates :clip_id, presence: true
