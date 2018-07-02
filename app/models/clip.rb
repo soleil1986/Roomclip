f00df1d471881ff85952be18bb3702b4d284da35
ohidetoshi <onuki_hidetoshi@onukihidetoshinoMacBook-puro.local>
2018-07-02 22:04:02 +0900

    前の投稿、次の投稿に行けるよう変更

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -6,4 +6,12 @@ class Clip < ApplicationRecord
   def like_user(user_id)
     likes.find_by(user_id: user_id)
   end
+
+  def previous
+    user.clips.order(id: :DESC).where("id < ?", id).first
+  end
+
+  def next
+    user.clips.order(id: :ASC).where("id > ?", id).first
+  end
 end
