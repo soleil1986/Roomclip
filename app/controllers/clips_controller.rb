c827c60b0f4ac4cc9c8fa38b63ff125069c9831c
ohidetoshi <onuki_hidetoshi@onukihidetoshinoMacBook-puro.local>
2018-07-03 10:26:38 +0900

    コメント追加_ 補足説明

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -13,8 +13,11 @@ class ClipsController < ApplicationController
 
   def show
     @clip = Clip.find(params[:id])
+    #このユーザの人気の部屋写真を表示するため
     @clips_like = Clip.where(user_id: @clip.user).order("likes_count DESC").limit(12)
+    #関連するインテリア実例を表示するため（タグ機能実装後、記述内容変更予定）
     @clips_tag = Clip.order("likes_count DESC").limit(12)
+    #このインテリアが撮影された日の人気写真を表示するため
     @clips_day =  Clip.where(created_at: @clip.created_at.all_day).order("likes_count DESC").limit(12)
   end
 
--- app/models/clip.rb
+++ app/models/clip.rb
@@ -7,10 +7,12 @@ class Clip < ApplicationRecord
     likes.find_by(user_id: user_id)
   end
 
+# clip詳細画面で、前の投稿に進むで使用
   def previous
     user.clips.order(id: :DESC).where("id < ?", id).first
   end
 
+# clip詳細画面で、次の投稿に進むで使用
   def next
     user.clips.order(id: :ASC).where("id > ?", id).first
   end
