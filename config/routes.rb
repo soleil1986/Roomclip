82e58c05deaa64f89d6c3ae90f6dae96789e4ae7
hiroaki <sanotaiyou@sanotaiyounoMacBook-puro.local>
2018-07-04 13:28:05 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,13 @@ Rails.application.routes.draw do
 
 # マージする前に不要な記述を削除する
   root "clips#index"
+  resources :relationships, only: [:create, :destroy]
   resources :users, only: [:show]
+    resources :users do
+      member do
+       get :follow, :follower
+      end
+  end
   resources :clips, only:[:index, :new, :create, :destroy, :show] do
   end
 end
