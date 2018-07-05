ac3fb54939a4c17525fe3bd0fd4ab95997047b84
ohidetoshi <onuki_hidetoshi@onukihidetoshinoMacBook-puro.local>
2018-07-05 20:10:45 +0900

    Userコントローラにuserphotoアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,11 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :clips, only:[:index, :new, :create, :destroy, :show] do
   end
+
+  resources :users do
+    member do
+      get 'userphoto'
+    end
+  end
+
 end
