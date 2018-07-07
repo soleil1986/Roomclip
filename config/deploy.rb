20507ef3beb22abc24fe01fbf0d24945b819e134
shukan0728 <cielo28verano@gmail.com>
2018-07-07 16:35:14 +0900

    デプロイ設定修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/shukannatsumi/Roomclip.pem']
+                  keys: ['/Users/shukannatsumi/.ssh/Roomclip.pem']
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
