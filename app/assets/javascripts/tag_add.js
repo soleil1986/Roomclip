3503ce481f7e3cd3935caa168dd78813882bf66e
shukan0728 <cielo28verano@gmail.com>
2018-07-08 16:18:09 +0900

    投稿時のプロフィールタグ追加処理

--- app/assets/javascripts/tag_add.js
+++ app/assets/javascripts/tag_add.js
@@ -1,4 +1,47 @@
 // タグ追加処理
+// $(function() {
+
+//  function buildTag(tag) {
+//     var html =`
+//     <div class="tag-back radius-min" data-tag="${tag}">${tag}
+//     <span id="remove-icon" class="fontello tag-close" data-tag="${tag}">x</span></div>`
+//     return html
+//   }
+
+//   $(function() {
+//     $('#Tag-input-button').on('click',function() {
+//       var tag = $('#Tag-input').val();
+//       var addTag = buildTag(tag);
+//       console.log(addTag)
+//       $('#before-input').css('display', 'none');
+//       $('#Tag-input-list').append(addTag);
+//       $('#Tag-input').val('');
+//       var a = $('#tag-send').val();
+//       if (a == "") {
+//         var b = tag
+//       } else {
+//         var b = a + "," + tag
+//       }
+//       $('#tag-send').val(b);
+//       });
+//     })
+
+//     $(document).on('click','#remove-icon', function() {
+//       $(this).parent().remove();
+//     });
+
+// // プロフィールタグ追加処理
+//   $(function() {
+//     $('.tag-back,radius-min,tag-insert').on('click',function() {
+//       var tag = $(this).data('tag');
+//       var addTag = buildTag(tag);
+//       $('#before-input').css('display', 'none');
+//       $('#Tag-input-list').html(addTag);
+//     });
+//   })
+// });
+
+
 $(function() {
 
  function buildTag(tag) {
@@ -9,9 +52,10 @@ $(function() {
   }
 
   $(function() {
-    $('#Tag-input-button').on('click',function(e) {
+    $('#Tag-input-button').on('click',function() {
       var tag = $('#Tag-input').val();
       var addTag = buildTag(tag);
+      console.log(addTag)
       $('#before-input').css('display', 'none');
       $('#Tag-input-list').append(addTag);
       $('#Tag-input').val('');
@@ -22,9 +66,31 @@ $(function() {
         var b = a + "," + tag
       }
       $('#tag-send').val(b);
+      });
     })
-  })
+
     $(document).on('click','#remove-icon', function() {
       $(this).parent().remove();
     });
+});
+
+// プロフィールタグ追加処理
+
+$(function() {
+
+ function buildProfileTag(tag) {
+    var html =`
+    <div class="profile tag-back radius-min" data-tag="${tag}">${tag}
+    <span id="remove-icon" class="fontello tag-close" data-tag="${tag}">x</span></div>`
+    return html
+  }
+  $(function() {
+    $('.tag-back,radius-min,tag-insert').on('click',function() {
+      var tag = $(this).data('tag');
+      var addTag = buildProfileTag(tag);
+      $('#before-input').css('display', 'none');
+      $('.profile,tag-back,radius-min').remove();
+      $('#Tag-input-list').append(addTag);
+    });
+  })
 });
