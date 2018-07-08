a5b95fd89e2777744cc9eb4044726e66da10a65e
shukan0728 <cielo28verano@gmail.com>
2018-07-08 18:11:56 +0900

    clip/show画面にタグ一覧表示

--- app/assets/javascripts/tag_add.js
+++ app/assets/javascripts/tag_add.js
@@ -1,46 +1,4 @@
-// タグ追加処理
-// $(function() {
-
-//  function buildTag(tag) {
-//     var html =`
-//     <div class="tag-back radius-min" data-tag="${tag}">${tag}
-//     <span id="remove-icon" class="fontello tag-close" data-tag="${tag}">x</span></div>`
-//     return html
-//   }
-
-//   $(function() {
-//     $('#Tag-input-button').on('click',function() {
-//       var tag = $('#Tag-input').val();
-//       var addTag = buildTag(tag);
-//       console.log(addTag)
-//       $('#before-input').css('display', 'none');
-//       $('#Tag-input-list').append(addTag);
-//       $('#Tag-input').val('');
-//       var a = $('#tag-send').val();
-//       if (a == "") {
-//         var b = tag
-//       } else {
-//         var b = a + "," + tag
-//       }
-//       $('#tag-send').val(b);
-//       });
-//     })
-
-//     $(document).on('click','#remove-icon', function() {
-//       $(this).parent().remove();
-//     });
-
-// // プロフィールタグ追加処理
-//   $(function() {
-//     $('.tag-back,radius-min,tag-insert').on('click',function() {
-//       var tag = $(this).data('tag');
-//       var addTag = buildTag(tag);
-//       $('#before-input').css('display', 'none');
-//       $('#Tag-input-list').html(addTag);
-//     });
-//   })
-// });
-
+// 選択タグ追加処理
 
 $(function() {
 
@@ -94,3 +52,4 @@ $(function() {
     });
   })
 });
+
