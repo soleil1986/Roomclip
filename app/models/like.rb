7267e88cb4749aaaa4c4a1ddc021f06be6339d4c
toshi-oliver <igpoid@gmail.com>
2018-07-11 15:00:57 +0900

    Likeモデルにアソシエーションを定義。またいいねカウントのカウンターキャッシュも記述

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,4 @@
 class Like < ApplicationRecord
-  belong_to :user, counter_cache: :likes_count
-  belong_to :clip, counter_cache: :likes_count
-
-  validates :user_id, presence: true
-  validates :clip_id, presence: true
+  belongs_to :user
+  belongs_to :clip, counter_cache: :likes_count
 end
