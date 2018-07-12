83e2007ce17d5a5540c25d0ed8e2480c55a52380
ohidetoshi <onuki_hidetoshi@onukihidetoshinoMacBook-puro.local>
2018-07-12 21:29:42 +0900

    dailiesのアクション内容を編集

--- app/controllers/dailies_controller.rb
+++ app/controllers/dailies_controller.rb
@@ -1,21 +1,39 @@
 class DailiesController < ApplicationController
 
+# show以外は管理者側の登録・変更・削除機能
+# showはユーザーの確認画面
   def new
+    @daily = Daily.new
   end
 
   def create
+    @daily = Daily.create
+    redirect_to edit_daily_path(@daily)
   end
 
   def show
+    @daily = Daily.find(params[:id])
   end
 
   def edit
+    @daily = Daily.find(params[:id])
   end
 
   def update
+    @daily = Daily.find(params[:id])
   end
 
   def destroy
+    daily = Daily.find(params[:id])
+    daily.destroy
+    redirect_to new_daily_path(@daily)
+  end
+
+private
+
+  def daily_params
+    params.required(:daily).permit(:date)
+
   end
 
 end
