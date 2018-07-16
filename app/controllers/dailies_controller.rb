da413215bd0c24d96317fb54534744c7b72062fc
ohidetoshi <onuki_hidetoshi@onukihidetoshinoMacBook-puro.local>
2018-07-16 13:45:42 +0900

    dailyclip登録のためのdailiesコントローラ編集
    newとcreateとshowを編集し仮実装

--- app/controllers/dailies_controller.rb
+++ app/controllers/dailies_controller.rb
@@ -4,15 +4,23 @@ class DailiesController < ApplicationController
 # showはユーザーの確認画面
   def new
     @daily = Daily.new
+    @daily.dailyclips.build
   end
 
   def create
-    @daily = Daily.create
+    @daily = Daily.create(daily_params)
     redirect_to edit_daily_path(@daily)
   end
 
   def show
     @daily = Daily.find(params[:id])
+    @dailyclips = @daily.dailyclips
+    @clip = @dailyclips.pluck(:clip_id)
+    @dailyphoto = Clip.where(id: @clip)
+    # @dailyでdaily_idを定義
+    # @dailyclipsでdailyclipsテーブルから@dailyで定義したidのデータを定義
+    # @clipで@dailyclipsで定義したデータの中からclip_idだけを定義
+    # @dailyphoyoでclipsテーブルから@clipで定義したclip_idのclipデータを定義。この時、id=@clipではなくid: @clipとシンボル型にしデータ取得
   end
 
   def edit
@@ -32,7 +40,7 @@ class DailiesController < ApplicationController
 private
 
   def daily_params
-    params.required(:daily).permit(:date)
+    params.required(:daily).permit(:date, dailyclips_attributes: [:id, :daily_id, :num, :clip_id])
 
   end
 
